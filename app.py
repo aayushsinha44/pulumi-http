a3a7aaba8e01dacd8cdba20360ab9d0fc698966e
Aayush Sinha <aayushsinha44@gmail.com>
2021-12-29 15:26:44 +0530

    gcp backend

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from pulumi_aws import s3
 from flask import Flask, request
 import uuid
 from flask_cors import CORS
+import pulumi_gcp as gcp
+from pulumi_gcp.storage.bucket import Bucket
+import json
 
 
 app = Flask(__name__)
@@ -35,6 +38,72 @@ def create_aws_s3(content):
                   })
   
   pulumi.export("website_url", bucket.website_endpoint)
+
+def create_gcp_bucket(content):
+  bucket = gcp.storage.Bucket("static-site",
+    cors=[gcp.storage.BucketCorArgs(
+      max_age_seconds=3600,
+      methods=[
+          "GET",
+          "HEAD",
+          "PUT",
+          "POST",
+          "DELETE",
+      ],
+      origins=["*"],
+      response_headers=["*"],
+    )],
+    force_destroy=True,
+    location="EU",
+    uniform_bucket_level_access=False,
+    website=gcp.storage.BucketWebsiteArgs(
+      main_page_suffix="index.html",
+      not_found_page="404.html",
+    ))
+  
+  file = gcp.storage.BucketObject("index",
+    bucket=bucket.id,
+    content=content)
+  
+  gcp.storage.BucketAccessControl("publicRule",
+    bucket=bucket.name,
+    role="READER",
+    entity="allUsers")
+
+  url = gcp.storage.get_object_signed_url(bucket=bucket.id,
+    path=file.name)
+  
+  pulumi.export("website_url", url['signed_url'])
+
+@app.route("/gcp/sites", methods=["POST"])
+def create_gcp_website():
+  stack_name = str(uuid.uuid4())
+  content = request.json.get('content')
+
+  credentials = request.json.get('credentials')
+
+  if credentials == None:
+    return {"message": "GCP credentials are required"}, 400
+
+  if content == None or content == "":
+    return {"message": "content required"}, 400
+
+  try:
+    def pulumi_progam():
+      return create_gcp_bucket(content)
+    
+    stack = auto.create_stack(stack_name=stack_name,
+                              project_name=PROJECT_NAME,
+                              program=pulumi_progam)
+    stack.set_config("gcp:project", auto.ConfigValue(credentials['project_id']))
+    stack.set_config("gcp:credentials", auto.ConfigValue(value=json.dumps(credentials)))
+
+    up_res = stack.up(on_output=print)
+    return {"id": stack_name, "website_url": up_res.outputs['website_url'].value}
+  except auto.StackAlreadyExistsError:
+    return {"message": "stack already exists"}, 400
+  except Exception as e:
+    return {"message": "something went wrong", "execption": str(e)}, 500
 
 @app.route("/aws/sites", methods=["POST"])
 def create_aws_website():
@@ -94,7 +163,35 @@ def delete_aws_website(id):
   except Exception as e:
     return  {"message": "something went wrong", "execption": str(e)}, 500
 
+@app.route("/gcp/sites/<string:id>", methods=["DELETE"])
+def delete_gcp_website(id):
+  stack_name = id
+
+  credentials = request.json.get('credentials')
+
+  if credentials == None:
+    return {"message": "GCP credentials are required"}, 400
+
+  try:
+    stack = auto.select_stack(stack_name=stack_name,
+                              project_name=PROJECT_NAME,
+                              program=lambda *args: None)
+    stack.set_config("gcp:project", auto.ConfigValue(credentials['project_id']))
+    stack.set_config("gcp:credentials", auto.ConfigValue(value=json.dumps(credentials)))
+
+    stack.destroy(on_output=print)
+    stack.workspace.remove_stack(stack_name)
+    return {"message": "deleted successfully"}
+    
+  except auto.StackNotFoundError:
+    return {"message": "stack not found"}, 400
+  except auto.ConcurrentUpdateError:
+    return {"message": "delete in progress"}, 400
+  except Exception as e:
+    return  {"message": "something went wrong", "execption": str(e)}, 500
+
 if __name__ == "__main__":
   workspace = auto.LocalWorkspace()
   workspace.install_plugin("aws", "v4.0.0")
+  workspace.install_plugin("gcp", "v6.6.0")
   app.run(host='0.0.0.0', port=5000, debug=True)
